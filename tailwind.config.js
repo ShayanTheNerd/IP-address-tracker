5ffa52d6eb6049211a9e97906207de27d5ccb79f
Shayan Zamani <sh.zamani384@gmail.com>
2023-10-01 23:12:41 +0330

    feat(typography): add Rubik font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
 	content: ['./index.html'],
 	future: { hoverOnlyWhenSupported: true },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
 };

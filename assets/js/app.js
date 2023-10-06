bce1606ae773dc31c322e19bbbfda3209dcbd2e1
Shayan Zamani <sh.zamani384@gmail.com>
2023-10-06 08:38:23 +0330

    feat: dislay IP data (fake) based on user input

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ import getClientIP from './modules/getClientIP.mjs';
 import displayIPData from './modules/displayIPData.mjs';
 
 const IPInput = document.getElementById('IP_input');
+const IPTrackerForm = document.getElementById('IP_tracker_form');
 
 window.addEventListener('load', async () => {
 	IPInput.focus();
@@ -15,5 +16,12 @@ window.addEventListener('load', async () => {
 	const clientIP = await getClientIP();
 	const IPData = await getIPData(clientIP);
 
+	displayIPData(IPData);
+});
+
+IPTrackerForm.addEventListener('submit', async event => {
+	event.preventDefault();
+
+	const IPData = await getIPData(IPInput.value);
 	displayIPData(IPData);
 });

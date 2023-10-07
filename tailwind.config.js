c42dbc761af549b106fbf358c528aa5d13338b98
Shayan Zamani <sh.zamani384@gmail.com>
2023-10-07 20:51:41 +0330

    fix: update minor styles & fix responsive issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,14 @@ export default {
 				grey: '#969696',
 				birch: '#2b2b2b',
 			},
-			opacity: {
-				85: '0.85',
-			},
 			zIndex: {
 				500: 500,
+			},
+			fontSize: {
+				xxs: '10px',
+			},
+			lineHeight: {
+				2: '12px',
 			},
 		},
 	},
